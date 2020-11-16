0a40387c23b24b15cfc1d6e534da89758f046cf4
noemivega <66137245+noemivega@users.noreply.github.com>
2020-11-16 09:26:51 -0600

    Ubicación con marcador

--- src/config/passport.js
+++ src/config/passport.js
@@ -18,10 +18,9 @@ module.exports = function (passport) {
 
   // Signup
   passport.use('local-signup', new LocalStrategy({
-    // by default, local strategy uses username and password, we will override with email
     usernameField: 'email',
     passwordField: 'password',
-    passReqToCallback : true // allows us to pass back the entire request to the callback
+    passReqToCallback : true 
   },
   function (req, email, password, done) {
     User.findOne({'local.email': email}, function (err, user) {
@@ -42,9 +41,6 @@ module.exports = function (passport) {
     });
   }));
 
-  // login
-  // we are using named strategies since we have one for login and one for signup
-  // by default, if there was no name, it would just be called 'local
   passport.use('local-login', new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
